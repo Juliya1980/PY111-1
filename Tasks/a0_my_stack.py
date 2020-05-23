a846ec7a6704cfe6b18b164440889548f5c0ce6e
@Julia1980 <vol-nina@yandex.ru>
2020-05-23 13:00:28 +0300

    try1

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -1,7 +1,7 @@
 """
 My little Stack
 """
-import random
+
 from typing import Any
 #test tamp help git.
 spis_1 = [1, 2, 3, 4, 5]
@@ -13,7 +13,7 @@ def push(elem: Any) -> None:
     :return: Nothing
     """
     global spis_1
-    spis_1.append(random.randint(10))
+    spis_1.append(elem)
     print(spis_1)
     return None
 
@@ -24,29 +24,39 @@ def pop() -> Any:
     :return: popped element
     """
     global spis_1
-    n = 0
-    for i in spis_1:
-        print(i)
-        spis_1.append(spis_1.pop(0))
-        n += 1
-    return None
+    if len(spis_1) >= 1:
+        el_0 = (spis_1[0])
+        del spis_1[0]
+        return el_0
+    else:
+        return None
 
 
 def peek(ind: int = 0) -> Any:
     """
     Allow you to see at the element in the stack without popping it.
-
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
-    print(ind)
-    return None
+    global spis_1
+    if ind < len(spis_1):
+        print(ind, spis_1[ind])
+        return spis_1[ind]
+    else:
+        return None
 
 
 def clear() -> None:
     """
     Clear my stack
-
     :return: None
     """
+    global spis_1
+    clear(spis_1)
+    print(spis_1)
     return None
+
+if __name__ == '__main__':
+    push(6)
+    pop()
+    peek(2)
